6f160bb88a4dbdb9c7bb718942eaf311dd8e3410
Logic-seeker <shobhabstha@gmail.com>
2012-01-18 13:32:48 +0545

    banners

--- app/admin/banners.rb
+++ app/admin/banners.rb
@@ -6,6 +6,17 @@ ActiveAdmin.register Banner do
         # Instance method
       end
 
+      def update
+        @banner = Banner.find(params[:id])
+        @banner.save!
+        uploader = ImageUploader.new
+        uploader.store!(params[:banner][:image_file])
+        @banner.caption = params[:banner][:caption]
+        @banner.image_path = uploader.to_s
+        @banner.save!
+        redirect_to admin_banners_path, :notice => "Banner updated successfully!"
+      end
+
       def create
         uploader = ImageUploader.new
         uploader.store!(params[:banner][:image_file])
