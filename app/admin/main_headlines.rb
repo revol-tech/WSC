8fe9d4cbf4d326ccbb0c7502bace9ddb3a380f7a
revol-tech <revolt.techno@gmail.com>
2012-01-30 18:33:19 +0545

    css of first page and about us

--- app/admin/main_headlines.rb
+++ app/admin/main_headlines.rb
@@ -1,3 +1,3 @@
 ActiveAdmin.register MainHeadline do
-  
+
 end
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,7 @@
 class MainController < ApplicationController
   def index
     @banners = Banner.all
-    @news_and_events = NewsAndEvent.order("created_at").page(params[:page]).per(2)
+    @news_and_events = NewsAndEvent.order("created_at").page(params[:page]).per(3)
     @activities = Activity.order("created_at").page(params[:page]).per(2)
     respond_to do |format|
       format.html
@@ -10,7 +10,7 @@ class MainController < ApplicationController
   end
 
   def news_paginator
-     @news_and_events = NewsAndEvent.order("created_at").page(params[:page]).per(2)
+     @news_and_events = NewsAndEvent.order("created_at").page(params[:page]).per(3)
      respond_to do |format|
       format.js
     end
