3e9091ef202e2388483ff612a484a4a29140bd1b
Logic-seeker <shobhabstha@gmail.com>
2012-02-13 15:17:55 +0545

    data seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+["hello|/uploads/images/IMG_0291.JPG","hello2|/uploads/images/IMG_0291.JPG"].each do |t|
+    headline, image_path = t.chomp.split("|")
+    Activity.create!(:headline => headline, :image_path => image_path)
+  end
+
+["banner1|/uploads/images/IMG_0291.JPG","banner2|/uploads/images/IMG_0291.JPG"].each do |t|
+    caption, image_path = t.chomp.split("|")
+    Banner.create!(:caption => caption, :image_path => image_path)
+  end
+
+["hello|/uploads/images/IMG_0291.JPG","hello2|/uploads/images/IMG_0291.JPG"].each do |t|
+    headline, image_path = t.chomp.split("|")
+    NewsAndEvent.create!(:headline => headline, :image_path => image_path)
+  end
