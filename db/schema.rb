afa50c4b9e129b7db818bfaf16419269c402cca4
ramesh <rdhami425@gmail.com>
2012-02-27 14:42:44 +0545

    grammar edit

--- db/schema.rb
+++ db/schema.rb
@@ -61,13 +61,6 @@ ActiveRecord::Schema.define(:version => 20120210161500) do
     t.datetime "updated_at"
   end
 
-  create_table "main_headlines", :force => true do |t|
-    t.string   "title"
-    t.string   "summary"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "news_and_events", :force => true do |t|
     t.string   "headline"
     t.string   "image_path"
