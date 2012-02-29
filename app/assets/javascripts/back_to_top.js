1644488cff181390248d75d4c46545a255f2699e
revol-tech <revolt.techno@gmail.com>
2012-02-29 12:21:04 +0545

    edited

--- app/assets/javascripts/back_to_top.js
+++ app/assets/javascripts/back_to_top.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-	    $('#back').click(
+	    $('.back_img').click(
 	        function (e) {
 	            $('html, body').animate({scrollTop: '0px'}, 800);
 	        });
